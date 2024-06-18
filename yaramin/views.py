5966c9ada801692a303448d4c59939ff0b4d6181
Renad <renadfilfilan3@gmail.com>
2024-06-19 00:53:39 +0300

    First Upload

---
+++ yaramin/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    templates = loader.get_template('base.html')
+    return HttpResponse(templates.render())
+
+def log_in(request):
+    templates = loader.get_template('login.html')
+    return HttpResponse(templates.render())
+
+def sign_up(request):
+    templates = loader.get_template('signup.html')
+    return HttpResponse(templates.render())
+
+def show_computers(request):
+    templates = loader.get_template('computers.html')
+    return HttpResponse(templates.render())
+
+def show_tablets(request):
+    templates = loader.get_template('tablets.html')
+    return HttpResponse(templates.render())
+
+def show_mobiles(request):
+    templates = loader.get_template('mobiles.html')
+    return HttpResponse(templates.render())
